bbec8e1195beb70fac31459e8ac2ed5f97b1f42c
iheartsylviegrace <sylviegrace.mp3@gmail.com>
2026-08-05 20:37:26 -0700

    Update create-paypal-order.js

--- api/create-paypal-order.js
+++ api/create-paypal-order.js
@@ -24,6 +24,15 @@ async function getPayPalAccessToken() {
 }
 
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "https://sylviegrace.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed"
